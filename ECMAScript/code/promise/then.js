07596c7a92761660cbe29fcf7dbbdf7be7c60ba5
ZhengAu <598359997@qq.com>
2020-02-10 22:59:59 +0800

    Promise.prototype.then()

---
+++ ECMAScript/code/promise/then.js
@@ -0,0 +1,24 @@
+function func() {
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			return resolve();
+		}, 1000);
+	})
+}
+
+func()
+	.then(() => {
+		console.log(11);
+	})
+	.then(() => {
+		console.log(22);
+	})
+	.then(() => {
+		return func();
+	})
+	.then(() => {
+		console.log(33);
+	})
+	.catch(() => {
+		console.log('fail...');
+	});
