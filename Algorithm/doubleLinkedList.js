236ecefe0f5265c18e8cf4f9ab3ed84879d097ac
ZhengAu <598359997@qq.com>
2020-07-25 16:12:59 +0800

    双向链表新增头尾插入方法

--- Algorithm/doubleLinkedList.js
+++ Algorithm/doubleLinkedList.js
@@ -28,13 +28,47 @@ class DoubleLinkedList {
 	 * @param {LinedNode} node
 	 * @returns {boolean}
 	 */
-	inertHead(node) {}
+	insertHead(node) {
+		if (!node) {
+			return false;
+		}
+
+		if (this.isEmpty()) {
+			this.head.next = node;
+			node.pre = this.head;
+		} else {
+			node.next = this.head.next;
+			this.head.next.pre = node;
+			this.head.next = node;
+			node.pre = this.head;
+		}
+		this.count++;
+		return true;
+	}
 
 	/**
 	 * @param {LinedNode} node
 	 * @returns {boolean}
 	 */
-	insertTail(node) {}
+	insertTail(node) {
+		if (!node) {
+			return false;
+		}
+
+		if (this.isEmpty()) {
+			this.head.next = node;
+			node.pre = this.head;
+		} else {
+			let curNode = this.head.next;
+			while (curNode.next) {
+				curNode = curNode.next;
+			}
+			curNode.next = node;
+			node.pre = curNode;
+		}
+		this.count++;
+		return true;
+	}
 
 	/**
 	 * @param {number} index
@@ -49,7 +83,32 @@ class DoubleLinkedList {
 	 */
 	deleteByIndex(index) {}
 
-	destroy() {}
+	destroy() {
+		this.head.next = null;
+		this.count = 0;
+	}
 
-	print() {}
+	print() {
+		let str = '';
+		let curNode = this.head.next;
+		while (curNode) {
+			str += curNode.val + ' ';
+			curNode = curNode.next;
+		}
+		console.log(str);
+	}
 }
+
+let list = new DoubleLinkedList();
+let node1 = new LinkedNode(11);
+let node2 = new LinkedNode(22);
+let node3 = new LinkedNode(33);
+let node4 = new LinkedNode(44);
+let node5 = new LinkedNode(55);
+list.insertHead(node3);
+list.insertHead(node1);
+list.insertHead(node2);
+list.insertTail(node5);
+list.insertTail(node4);
+console.log(list);
+list.print();
