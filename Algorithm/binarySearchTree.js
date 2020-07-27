65a657d6ca5afa530299a687de4960c3d385d3f7
ZhengAu <598359997@qq.com>
2020-07-27 19:03:45 +0800

    二叉搜索树更新

--- Algorithm/binarySearchTree.js
+++ Algorithm/binarySearchTree.js
@@ -2,8 +2,27 @@
  * 二叉搜索树
  */
 
+class BSTNode {
+	constructor(value, left, right) {
+		this.value = value ? value : 0;
+		this.left = left ? left : null;
+		this.right = right ? right : null;
+	}
+}
+
 class BinarySearchTree {
-	constructor() {}
+	constructor() {
+		this.root = null;
+		this.count = 0;
+	}
+
+	isEmpty() {
+		return this.count === 0;
+	}
+
+	size() {
+		return this.count;
+	}
 
 	insert(value) {}
 
