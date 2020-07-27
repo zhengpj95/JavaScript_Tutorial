0bb2a3fca5278e5d6761875803693634384035d1
ZhengAu <598359997@qq.com>
2020-07-27 18:31:30 +0800

    添加二叉搜索树

---
+++ Algorithm/binarySearchTree.js
@@ -0,0 +1,26 @@
+/**
+ * 二叉搜索树
+ */
+
+class BinarySearchTree {
+	constructor() {}
+
+	insert(value) {}
+
+	remove(value) {}
+
+	/**前序遍历*/
+	preOrderTraversal() {}
+
+	/**中序遍历*/
+	inOrderTraversal() {}
+
+	/**后序遍历*/
+	postOrderTraversal() {}
+
+	maxValue() {}
+
+	minValue() {}
+
+	destroy() {}
+}
