c71ee8be4258a2ab77c2242aa206515fae5b0c8c
ZhengAu <598359997@qq.com>
2020-07-31 17:34:37 +0800

    红点系统添加保存方法

--- Algorithm/redpointSystem.js
+++ Algorithm/redpointSystem.js
@@ -11,7 +11,19 @@ class RedPointSystem {
 	 * @param {number | boolean} value
 	 * @param  {...any} params
 	 */
-	saveData(value, ...params) {}
+	saveData(value, ...params) {
+		let newValue = !!value; // convert to boolean
+
+		if (params.length) {
+			let keys = [];
+			for (let i = 0; i < params.length; i++) {
+				keys.push(this.getObjectName(params[i]));
+			}
+			this.write(newValue, this.datas, 0, keys);
+		} else {
+			console.log(`RedPointSystem.saveData 保存设置参数错误，至少需要一个参数`);
+		}
+	}
 
 	/**
 	 * @param {boolean} value
@@ -19,7 +31,25 @@ class RedPointSystem {
 	 * @param {number} index
 	 * @param {string[]} keyList
 	 */
-	write(value, obj, index, keyList) {}
+	write(value, obj, index, keyList) {
+		let key = keyList[index];
+
+		if (index < keyList.length - 1) {
+			if (!obj[key]) {
+				obj[key] = {};
+			} else if (typeof obj[key] != 'object') {
+				obj[key] = {};
+				console.log(keyList.slice(0, index + 1), `本层已设置为值，无法再设置下一层`);
+			}
+			this.write(value, obj[key], index + 1, keyList);
+		} else {
+			if (typeof obj[key] != 'object') {
+				obj[key] = value;
+			} else {
+				console.log(keyList, `下层有值，不可设置`);
+			}
+		}
+	}
 
 	/**
 	 * @param  {...any} params
@@ -45,5 +75,26 @@ class RedPointSystem {
 	 * @param {any} obj
 	 * @returns {string}
 	 */
-	getObjectName(obj) {}
+	getObjectName(obj) {
+		let result = '';
+		if (typeof obj === 'number') {
+			result = obj + '';
+		} else if (typeof obj === 'string') {
+			result = obj;
+		} else if (typeof obj === 'function') {
+			result = obj.name;
+		} else if (typeof obj === 'object') {
+			let prototype = obj.prototype ? obj.prototype : Object.getPrototypeOf(obj);
+			if (prototype && prototype.name) {
+				result = prototype.name;
+			} else {
+				let constructorStr = prototype.constructor.toString().trim();
+				let idx = constructorStr.indexOf('(');
+				result = constructorStr.substring(9, idx);
+			}
+		} else {
+			console.log(`需要获取的对象类型错误 ${obj}`);
+		}
+		return result;
+	}
 }
