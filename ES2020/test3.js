6dbddbdb55727b9cd4157ec023b52b094500f127
ZhengAu <598359997@qq.com>
2020-10-22 17:36:41 +0800

    ES2020新特性

---
+++ ES2020/test3.js
@@ -0,0 +1,17 @@
+/**
+ * BigInt
+ */
+
+let num = Number.MAX_SAFE_INTEGER;
+console.log(num);
+num += 1;
+console.log(num);
+num += 1;
+console.log(num);
+console.log('---------------------');
+num = 9007199254740991n;
+console.log(num);
+num++;
+num++;
+num++;
+console.log(num);
